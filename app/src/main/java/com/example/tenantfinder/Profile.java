960e66810496e79a4abc3a02f5c1510eb0c1ab84
savitore <21je0482@iitism.ac.in>
2022-04-18 23:45:56 +0530

    Almost done3.

--- app/src/main/java/com/example/tenantfinder/Profile.java
+++ app/src/main/java/com/example/tenantfinder/Profile.java
@@ -6,8 +6,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class Profile extends AppCompatActivity {
     private RecyclerView recyclerView;
+    MyAdapter1 myAdapter;
+    ArrayList<User1> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,9 @@ public class Profile extends AppCompatActivity {
         recyclerView=(RecyclerView) findViewById(R.id.recyclerview1);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        list= new ArrayList<User1>();
+        myAdapter=new MyAdapter1(this,list);
+        recyclerView.setAdapter(myAdapter);
+
     }
 }
